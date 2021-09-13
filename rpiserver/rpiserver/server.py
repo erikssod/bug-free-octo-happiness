c878d3506b3d2e1ee3dafad7c29c1aa025046aab
Daniel Eriksson <erikssod@users.noreply.github.com>
2021-09-13 22:09:07 +1000

    Working multi-input

--- rpiserver/rpiserver/server.py
+++ rpiserver/rpiserver/server.py
@@ -7,37 +7,41 @@ GPIO.setwarnings(False)
 
 
 class Valve(BaseModel):
-    gpio: int
+    gpio: list
     is_open: bool = False
 
     def init(self):
-            GPIO.setup(self.gpio, GPIO.OUT)
-            return self
+        for gpio in self.gpio:
+            GPIO.setup(gpio, GPIO.OUT)
+            GPIO.output(gpio, GPIO.HIGH) # Frivolous startup blink
+            GPIO.output(gpio, GPIO.LOW)  # -""-
+        return self
 
-    def open(self):
-            GPIO.output(self.gpio, GPIO.HIGH)
+    def open(self, gpio):
+            GPIO.output(gpio, GPIO.HIGH)
             self.is_open = True
 
-    def close(self):
-            GPIO.output(self.gpio, GPIO.LOW)
+    def close(self, gpio):
+            GPIO.output(gpio, GPIO.LOW)
             self.is_open = False
 
-valve = Valve(gpio=19).init()
+valve = Valve(gpio=[19,20,21]).init()
 
 valve_app = FastAPI()
 
 @valve_app.on_event("shutdown")
 def close_valves():
-    valve.close()
+    valve.close(19)
+    valve.close(20)
+    valve.close(21)
 
 @valve_app.get("/valves/open", response_model=Valve)
-#def open_valve(id:int):
-def open_valve():
-    valve.open()
+def open_valve(id:int):
+    valve.open(id)
     return valve
 
 @valve_app.get("/valves/close", response_model=Valve)
-def close_valve():
-    valve.close()
+def close_valve(id:int):
+    valve.close(id)
     return valve
 
